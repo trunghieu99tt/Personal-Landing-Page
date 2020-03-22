7bef4ee90aa20cf20b371454b8faf6d8fc4630b1
Hieu <trunghieu99tt@gmail.com>
2020-03-22 11:18:57 +0700

    Updates

---
+++ precache-manifest.e8f9ef671f166a60334441cc4768ced0.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b382d3d0c386325daa4d671eb2ed7894",
+    "url": "/personal-landing-page/index.html"
+  },
+  {
+    "revision": "e60d8b8de2b1358d145b",
+    "url": "/personal-landing-page/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "4796d485f6dd7cd34167",
+    "url": "/personal-landing-page/static/css/main.e2115443.chunk.css"
+  },
+  {
+    "revision": "e60d8b8de2b1358d145b",
+    "url": "/personal-landing-page/static/js/2.70e22f74.chunk.js"
+  },
+  {
+    "revision": "31634dc4df25202fc93a489309193edb",
+    "url": "/personal-landing-page/static/js/2.70e22f74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4796d485f6dd7cd34167",
+    "url": "/personal-landing-page/static/js/main.5b58b953.chunk.js"
+  },
+  {
+    "revision": "9c9ab67bd75b13ffe73d",
+    "url": "/personal-landing-page/static/js/runtime-main.f159a279.js"
+  },
+  {
+    "revision": "eb8506f8c1a3ffdb68eb01163b108a51",
+    "url": "/personal-landing-page/static/media/chrismas.eb8506f8.PNG"
+  },
+  {
+    "revision": "52287b924954a9467944a1d5149970c4",
+    "url": "/personal-landing-page/static/media/clothing-shop.52287b92.PNG"
+  },
+  {
+    "revision": "e413fca290a56f16a129e7cc34534245",
+    "url": "/personal-landing-page/static/media/convert.e413fca2.jpg"
+  },
+  {
+    "revision": "f862e614d5728680b867b0ee4e05189d",
+    "url": "/personal-landing-page/static/media/cosmeticshop.f862e614.PNG"
+  },
+  {
+    "revision": "9b683e0f5a2acaa690bca159a1619e8f",
+    "url": "/personal-landing-page/static/media/img1.9b683e0f.jpg"
+  },
+  {
+    "revision": "34ae37426292cd3c7c69a487045491bf",
+    "url": "/personal-landing-page/static/media/img2.34ae3742.jpg"
+  },
+  {
+    "revision": "91e331070549f8cace0ea56929219f1e",
+    "url": "/personal-landing-page/static/media/img3.91e33107.jpg"
+  },
+  {
+    "revision": "f74dceb68b56ec63485c9bc093e3b9a9",
+    "url": "/personal-landing-page/static/media/img4.f74dceb6.png"
+  },
+  {
+    "revision": "abd36a9dea479139bbc9604d907023d7",
+    "url": "/personal-landing-page/static/media/img5.abd36a9d.jpg"
+  },
+  {
+    "revision": "0a087c57ebcc9faf9c47a8f5e55698a5",
+    "url": "/personal-landing-page/static/media/img6.0a087c57.jpg"
+  },
+  {
+    "revision": "86602809f305e0d26b2ca5318437b1db",
+    "url": "/personal-landing-page/static/media/logo.86602809.png"
+  },
+  {
+    "revision": "12fae668b4bcbf29f2cd4d7dfa933324",
+    "url": "/personal-landing-page/static/media/memory-card.12fae668.PNG"
+  },
+  {
+    "revision": "f51395efe4af554c1666d23b3647ae01",
+    "url": "/personal-landing-page/static/media/music-player.f51395ef.PNG"
+  },
+  {
+    "revision": "3676862c648d154b0002aaffefd1111c",
+    "url": "/personal-landing-page/static/media/nbm.3676862c.jpg"
+  },
+  {
+    "revision": "ab135e3045f9c81a47e0b850fe550b13",
+    "url": "/personal-landing-page/static/media/nhathuoc.ab135e30.PNG"
+  },
+  {
+    "revision": "a3d9653c0b1d111b30c541ded92f03b6",
+    "url": "/personal-landing-page/static/media/omnifood.a3d9653c.PNG"
+  },
+  {
+    "revision": "5b075c5012653f06e551c5dbe3c3e0d1",
+    "url": "/personal-landing-page/static/media/pixel-thought.5b075c50.PNG"
+  },
+  {
+    "revision": "fdf03ff9312b9e43227e78e52a75ba87",
+    "url": "/personal-landing-page/static/media/tindog.fdf03ff9.PNG"
+  },
+  {
+    "revision": "f31ec2a1e86a86c5d47f7d5e45a8b941",
+    "url": "/personal-landing-page/static/media/todo.f31ec2a1.PNG"
+  },
+  {
+    "revision": "522162da9c13f9d5fa69f1df86a52b75",
+    "url": "/personal-landing-page/static/media/vgasoft.522162da.PNG"
+  },
+  {
+    "revision": "b55b994ca51036e508c2e7201689b58b",
+    "url": "/personal-landing-page/static/media/videoblog.b55b994c.PNG"
+  }
+]);
