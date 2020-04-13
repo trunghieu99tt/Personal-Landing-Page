48a95b2aae6cfa866f49109542fa1d6cd1a8dd56
Hieu <trunghieu99tt@gmail.com>
2020-04-13 17:16:29 +0700

    Updates

---
+++ precache-manifest.22d39ac2ff958783875dcd9ae2a110bf.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ae5e017098f88856688278bc2421512",
+    "url": "/trunghieu99tt.github.io/index.html"
+  },
+  {
+    "revision": "e60d8b8de2b1358d145b",
+    "url": "/trunghieu99tt.github.io/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "5ca60565ed46265fefb2",
+    "url": "/trunghieu99tt.github.io/static/css/main.e2115443.chunk.css"
+  },
+  {
+    "revision": "e60d8b8de2b1358d145b",
+    "url": "/trunghieu99tt.github.io/static/js/2.70e22f74.chunk.js"
+  },
+  {
+    "revision": "31634dc4df25202fc93a489309193edb",
+    "url": "/trunghieu99tt.github.io/static/js/2.70e22f74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ca60565ed46265fefb2",
+    "url": "/trunghieu99tt.github.io/static/js/main.cbc5f9ca.chunk.js"
+  },
+  {
+    "revision": "ac0a0fcb6b30667cc606",
+    "url": "/trunghieu99tt.github.io/static/js/runtime-main.71e02acf.js"
+  },
+  {
+    "revision": "5e005f87df2b2e7df481fa9215f89736",
+    "url": "/trunghieu99tt.github.io/static/media/chrismas-min.5e005f87.PNG"
+  },
+  {
+    "revision": "43cf6051d6225b2459f90feae57f6cbe",
+    "url": "/trunghieu99tt.github.io/static/media/clothing-shop-min.43cf6051.PNG"
+  },
+  {
+    "revision": "2c7c31062d378b9360c21c38c3e7d4dc",
+    "url": "/trunghieu99tt.github.io/static/media/cosmeticshop-min.2c7c3106.PNG"
+  },
+  {
+    "revision": "9b683e0f5a2acaa690bca159a1619e8f",
+    "url": "/trunghieu99tt.github.io/static/media/img1.9b683e0f.jpg"
+  },
+  {
+    "revision": "34ae37426292cd3c7c69a487045491bf",
+    "url": "/trunghieu99tt.github.io/static/media/img2.34ae3742.jpg"
+  },
+  {
+    "revision": "0b77d168b54563a4febdd37532c9445f",
+    "url": "/trunghieu99tt.github.io/static/media/img4-min.0b77d168.png"
+  },
+  {
+    "revision": "abd36a9dea479139bbc9604d907023d7",
+    "url": "/trunghieu99tt.github.io/static/media/img5.abd36a9d.jpg"
+  },
+  {
+    "revision": "0a087c57ebcc9faf9c47a8f5e55698a5",
+    "url": "/trunghieu99tt.github.io/static/media/img6.0a087c57.jpg"
+  },
+  {
+    "revision": "86602809f305e0d26b2ca5318437b1db",
+    "url": "/trunghieu99tt.github.io/static/media/logo.86602809.png"
+  },
+  {
+    "revision": "6c63a1aa951ff21d4bd258810e0a8359",
+    "url": "/trunghieu99tt.github.io/static/media/memory-card-min.6c63a1aa.PNG"
+  },
+  {
+    "revision": "bd9550df4e9e05637ab2be089604358e",
+    "url": "/trunghieu99tt.github.io/static/media/music-player-min.bd9550df.PNG"
+  },
+  {
+    "revision": "54c1714b0cbb5daf0a2ccce69ce6df82",
+    "url": "/trunghieu99tt.github.io/static/media/nhathuoc-min.54c1714b.PNG"
+  },
+  {
+    "revision": "a2fca4e3cb8c983aea873fa7e7354226",
+    "url": "/trunghieu99tt.github.io/static/media/omnifood-min.a2fca4e3.PNG"
+  },
+  {
+    "revision": "8e8947cac5cda1011ec02693ea9566b9",
+    "url": "/trunghieu99tt.github.io/static/media/pixel-thought-min.8e8947ca.PNG"
+  },
+  {
+    "revision": "bde16213c07f0d95f5bcc3e5d93d5946",
+    "url": "/trunghieu99tt.github.io/static/media/tindog-min.bde16213.PNG"
+  },
+  {
+    "revision": "68e1267b56517cc5aad624745f371cc7",
+    "url": "/trunghieu99tt.github.io/static/media/todo-min.68e1267b.PNG"
+  },
+  {
+    "revision": "c7c10ad1b010b3160a38db973f20a90e",
+    "url": "/trunghieu99tt.github.io/static/media/vgasoft-min.c7c10ad1.PNG"
+  },
+  {
+    "revision": "6205c6636c02e22694925147624be9c1",
+    "url": "/trunghieu99tt.github.io/static/media/videoblog-min.6205c663.PNG"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.749823a28392bc4a2d5692ee4958bc0e.js"
+  "/trunghieu99tt.github.io/precache-manifest.22d39ac2ff958783875dcd9ae2a110bf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/trunghieu99tt.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
