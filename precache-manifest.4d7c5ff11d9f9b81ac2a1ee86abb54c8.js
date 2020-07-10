8a627a37ffb3f72c2302534a31ecae6052f317fd
Rikikudo <trunghieu99tt@gmail.com>
2020-07-10 12:55:16 +0700

    Updates

---
+++ precache-manifest.4d7c5ff11d9f9b81ac2a1ee86abb54c8.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "677ac39f27d8510e4838c378f4b5fa52",
+    "url": "/Personal-Landing-Page/index.html"
+  },
+  {
+    "revision": "c2bbf16de091ebb97ae9",
+    "url": "/Personal-Landing-Page/static/css/2.7038f4e8.chunk.css"
+  },
+  {
+    "revision": "bb64a4f0c98ab64aed66",
+    "url": "/Personal-Landing-Page/static/css/main.2f7818f0.chunk.css"
+  },
+  {
+    "revision": "c2bbf16de091ebb97ae9",
+    "url": "/Personal-Landing-Page/static/js/2.4bbd1d9b.chunk.js"
+  },
+  {
+    "revision": "33cbcd6a9390ceaf67075c7d62c4c7e3",
+    "url": "/Personal-Landing-Page/static/js/2.4bbd1d9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb64a4f0c98ab64aed66",
+    "url": "/Personal-Landing-Page/static/js/main.02dea821.chunk.js"
+  },
+  {
+    "revision": "d5ab8f74c8094a911c22",
+    "url": "/Personal-Landing-Page/static/js/runtime-main.9cd288dc.js"
+  },
+  {
+    "revision": "cd639cf19ef51d164f493471f1a94a42",
+    "url": "/Personal-Landing-Page/static/media/banner1.cd639cf1.svg"
+  },
+  {
+    "revision": "43cf6051d6225b2459f90feae57f6cbe",
+    "url": "/Personal-Landing-Page/static/media/clothing-shop-min.43cf6051.PNG"
+  },
+  {
+    "revision": "2c7c31062d378b9360c21c38c3e7d4dc",
+    "url": "/Personal-Landing-Page/static/media/cosmeticshop-min.2c7c3106.PNG"
+  },
+  {
+    "revision": "559e0153a4bea253feb31a4db9be64b5",
+    "url": "/Personal-Landing-Page/static/media/github-jobs.559e0153.png"
+  },
+  {
+    "revision": "9b683e0f5a2acaa690bca159a1619e8f",
+    "url": "/Personal-Landing-Page/static/media/img1.9b683e0f.jpg"
+  },
+  {
+    "revision": "34ae37426292cd3c7c69a487045491bf",
+    "url": "/Personal-Landing-Page/static/media/img2.34ae3742.jpg"
+  },
+  {
+    "revision": "0b77d168b54563a4febdd37532c9445f",
+    "url": "/Personal-Landing-Page/static/media/img4-min.0b77d168.png"
+  },
+  {
+    "revision": "abd36a9dea479139bbc9604d907023d7",
+    "url": "/Personal-Landing-Page/static/media/img5.abd36a9d.jpg"
+  },
+  {
+    "revision": "0a087c57ebcc9faf9c47a8f5e55698a5",
+    "url": "/Personal-Landing-Page/static/media/img6.0a087c57.jpg"
+  },
+  {
+    "revision": "5f4d38d45a7e4fb1ad32864c0961a78a",
+    "url": "/Personal-Landing-Page/static/media/lazada-pet.5f4d38d4.png"
+  },
+  {
+    "revision": "86602809f305e0d26b2ca5318437b1db",
+    "url": "/Personal-Landing-Page/static/media/logo.86602809.png"
+  },
+  {
+    "revision": "6c63a1aa951ff21d4bd258810e0a8359",
+    "url": "/Personal-Landing-Page/static/media/memory-card-min.6c63a1aa.PNG"
+  },
+  {
+    "revision": "bd9550df4e9e05637ab2be089604358e",
+    "url": "/Personal-Landing-Page/static/media/music-player-min.bd9550df.PNG"
+  },
+  {
+    "revision": "54c1714b0cbb5daf0a2ccce69ce6df82",
+    "url": "/Personal-Landing-Page/static/media/nhathuoc-min.54c1714b.PNG"
+  },
+  {
+    "revision": "a2fca4e3cb8c983aea873fa7e7354226",
+    "url": "/Personal-Landing-Page/static/media/omnifood-min.a2fca4e3.PNG"
+  },
+  {
+    "revision": "0fb36d175c618616d74bac8740f25c4f",
+    "url": "/Personal-Landing-Page/static/media/portfolio-page.0fb36d17.png"
+  },
+  {
+    "revision": "427f803f3e21822f1d268c98c62f418e",
+    "url": "/Personal-Landing-Page/static/media/tigren-logo.427f803f.png"
+  },
+  {
+    "revision": "c7c10ad1b010b3160a38db973f20a90e",
+    "url": "/Personal-Landing-Page/static/media/vgasoft-min.c7c10ad1.PNG"
+  },
+  {
+    "revision": "6205c6636c02e22694925147624be9c1",
+    "url": "/Personal-Landing-Page/static/media/videoblog-min.6205c663.PNG"
+  },
+  {
+    "revision": "413be083d00c7308292ce92b5c09d1a7",
+    "url": "/Personal-Landing-Page/static/media/weather-app.413be083.png"
+  }
+]);
